db312359bc7ffbad0386394b70442d869e7c7cbc
Rustam Mamatov <Mamatovr31@gmail.com>
2026-01-12 19:55:16 +0200

    error correction flake

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -27,7 +32,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price (self, car: Car) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         difference = (self.clean_power - car.clean_mark)
         if difference <= 0:
             return 0.0
@@ -45,4 +50,3 @@ class CarWashStation:
         self.count_of_ratings += 1
         new_average_rating = new_rating / self.count_of_ratings
         self.average_rating = round(new_average_rating, 1)
-
